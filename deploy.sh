a06d983a2c57fb66e7ef1b4868f6cc69af17bc4b
Mathieu Jobin <thatcodingguy@gmail.com>
2020-07-06 20:14:58 -0700

    Invalidating index.html on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 hugo -D
 aws s3 sync public s3://blogmathieu
 aws cloudfront create-invalidation --distribution-id EHWGVX80UCBD1 --path /*
+aws cloudfront create-invalidation --distribution-id EHWGVX80UCBD1 --path /
